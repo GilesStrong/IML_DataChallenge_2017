3d08ca2367453624b32d91a457d4546eff4eac67
GilesStrong <giles.strong@outlook.com>
2018-03-14 16:11:56 +0000

    HL feature filtering

--- Modules/Class_Features.py
+++ Modules/Class_Features.py
@@ -1,1 +1,2 @@
 basic_features = ['jetPt','jetEta','jetMass','ntracks','ntowers']
+filtered_features_0 = ['ntracks', 'ntowers', 'jetMass', 'jetPt', 'max_track_pT', 'max_track_eta', 'min_track_eta', 'sum_tower_Ehad', 'max_tower_E', 'sum_track_pT', 'mean_tower_Eem', 'mean_track_pT', 'tower_Eem_frac', 'min_track_pT', 'sum_tower_Eem', 'min_tower_E', 'jetPhi', 'sum_tower_E', 'min_tower_eta', 'tower_Ehad_frac', 'max_tower_Eem', 'mean_tower_E', 'max_tower_Ehad', 'max_tower_eta', 'mean_tower_eta', 'jetEta', 'mean_tower_Ehad', 'mean_track_eta', 'min_tower_Eem']
